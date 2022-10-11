fe7a0cb0f9ba4c7e45087a726c00a1df65728672
Michele Ferracin <michele.ferracin@live.it>
2022-10-11 14:36:06 +0200

    Aggiunge 03 Routing

---
+++ 03-Routing/Publisher/Publisher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+class Publisher
+{
+    private const string Exchange_Topolino = "topolino";
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Scrivi numeri:");
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            // Dichiaro l'exchange dove pubblico i miei messaggi e con impostazione Fanout.
+            channel.ExchangeDeclare(exchange: Exchange_Topolino, type: ExchangeType.Direct);
+
+            while (true)
+            {
+                var message = Console.ReadLine();
+                var pariDispari = int.Parse(message) % 2 == 0 ? "pari" : "dispari";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: Exchange_Topolino,
+                                     routingKey: pariDispari,
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
---
+++ 03-Routing/Subscriber/Subscriber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+class Subscriber
+{
+    private const string Exchange_Topolino = "topolino";
+
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+
+        Console.WriteLine("Routing key:");
+        var routingKey = Console.ReadLine();
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            // Dichiaro quale exchange ascolto.
+            channel.ExchangeDeclare(exchange: Exchange_Topolino, type: ExchangeType.Direct);
+
+            // Dichiara un nome random per la coda dove riceverò i miei messaggi in abbonamento.
+            var queueName = channel.QueueDeclare(queue: "").QueueName;
+            channel.QueueBind(queue: queueName, exchange: Exchange_Topolino, routingKey: routingKey);
+
+            Console.WriteLine(" [*] Waiting for topolino.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                byte[] body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] {0}", message);
+            };
+            channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
